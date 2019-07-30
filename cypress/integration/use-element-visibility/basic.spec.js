708c62cce934d4a8042845d5182cef6abe46d6e8
Sanghyeon Lee <yongdamsh@gmail.com>
2019-07-30 21:55:45 +0900

    Minor changes for basic example

--- cypress/integration/use-element-visibility/basic.spec.js
+++ cypress/integration/use-element-visibility/basic.spec.js
@@ -1,18 +1,19 @@
 /// <reference types="Cypress" />
 
-beforeEach(() => {
-  cy.visit('/packages/use-element-visibility/examples/basic.html');
-});
 
 describe('basic use case', () => {
+  beforeEach(() => {
+    cy.visit('/packages/use-element-visibility/examples/basic.html');
+  });
+
   it('should display a red box', () => {
     cy.get("[data-cy=red-box]").should('exist');
-    cy.get("[data-cy=message]").should('contain', '0.0% crossed');
+    cy.get("[data-cy=message]").should('contain', 'The red box is about 0.0');
   });
 
   it('shows 100% crossed message', () => {
     cy.scrollTo('bottom');
 
-    cy.get('[data-cy=message]').should('contain', '100.0% crossed');
+    cy.get('[data-cy=message]').should('contain', 'The red box is about 100');
   });
 });
